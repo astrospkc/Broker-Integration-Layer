b29dcee8ee4e2c701ae37589390f7640311961cc
Punam <punamkumari399@gmail.com>
2025-11-08 17:58:16 +0530

    set up of rate-limit

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,24 @@ import express from "express"
 import brokerRouter from "./router/router.broker"
 import userRouter from "./router/router.user"
 import fakeTradeRouter from "./router/router.faketrade"
+import rateLimit from "express-rate-limit"
 const app = express()
 import dotenv from 'dotenv'
 import connectToMongo from "./connection/connection.mongodb"
 dotenv.config()
 connectToMongo()
+
+
+// Create a rate limiter instance
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 50,
+    message: 'Too many requests from this IP, please try again after 15 minutes',
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+app.use(limiter)
 
 app.use(express.json())
 app.use('/api', brokerRouter)
